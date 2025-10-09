da1c7fcf74028414c4d7b0e1e5ded6e5bd24136f
Nasstrom33 <nasstrom.alex@gmail.com>
2025-10-09 11:34:49 +0200

    setup apptheme

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,36 @@
 //Välj hushåll
+import { useAppTheme } from "@/constants/app-theme";
 import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
+  const theme = useAppTheme();
   return (
-    <TouchableOpacity
-      onPress={() => router.push({ pathname: "/housePage/sysslor" })}
+    //example with theme.
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Text>Välj hushåll</Text>
-    </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          padding: 20,
+          backgroundColor: theme.colors.primary,
+          borderRadius: 10,
+        }}
+        onPress={() => router.push({ pathname: "/housePage/sysslor" })}
+      >
+        <Text
+          style={{
+            color: theme.colors.onPrimary,
+          }}
+        >
+          Välj hushåll
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 }
