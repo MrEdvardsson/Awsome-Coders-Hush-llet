fabb5349efe7d39537db1b94a4b247a279656609
Nasstrom33 <nasstrom.alex@gmail.com>
2025-10-10 15:37:25 +0200

    setup authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,54 @@
 import { darkTheme, lightTheme } from "@/constants/app-theme";
+import { useReactQuerySetup } from "@/hooks/use-react-query-setup";
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import { auth } from "@/firebase-config";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthCacheListener } from "@/auth";
+import React from "react";
+const queryClient = new QueryClient();
 export default function RootLayout() {
+  // LogInFireBase();
   const colorScheme = useColorScheme();
   const theme = colorScheme === "dark" ? darkTheme : lightTheme;
+  useReactQuerySetup();
+
   return (
-    <PaperProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="housePage/houseHoldScreen"
-          options={{ title: "Hushåll", headerShown: false }}
-        />
-      </Stack>
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider theme={theme}>
+        {/* En komponent som lyssnar på auth förändringar */}
+        <AuthCacheListener />
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="housePage/houseHoldScreen"
+            options={{ title: "Hushåll", headerShown: false }}
+          />
+        </Stack>
+      </PaperProvider>
+    </QueryClientProvider>
   );
+}
+
+// Endast en test funktion för att testa om inloggningen fungerar.
+// Har skapat en användare i Firebase Console med dessa uppgifter.
+// Så om ni vill testa så kan nu köra denn funktionen vid uppstart, lägg till dessa i er egna databas.
+// Och kör funktionen LogInFireBase(); Vid uppstart av appen.
+async function LogInFireBase() {
+  const email = "alexander@gmail.com";
+  const password = "alexander123";
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("Inloggad användare:", user);
+  } catch (error) {
+    console.error("Fel vid inloggning:", error);
+  }
 }
---
+++ auth.ts
@@ -0,0 +1,42 @@
+// Ska returnera en query från tAnstack react query som kollar om användaren är inloggad eller inte
+import { onAuthStateChanged, type User } from "@firebase/auth";
+import { auth } from "@/firebase-config";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+export const AUTH_KEY = ["auth", "user"] as const;
+
+// En hook som läser auth läget.
+export function useAuthUser() {
+  const query = useQuery({
+    queryKey: AUTH_KEY,
+    queryFn: () => {
+      return new Promise<User | null>((resolve) => {
+        const currentUser = auth.currentUser;
+        if (currentUser) {
+          return resolve(currentUser);
+        } else {
+          const unsubscribe = onAuthStateChanged(auth, (userOrNull) => {
+            unsubscribe();
+            resolve(userOrNull ? userOrNull : null);
+          });
+        }
+      });
+    },
+    staleTime: Infinity,
+    gcTime: Infinity,
+  });
+  return query;
+}
+// En komponent som lyssnar på authentication förändringar tex vid inloggning och utloggning
+export function AuthCacheListener() {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      const value = user ? user : null;
+      queryClient.setQueryData(AUTH_KEY, value);
+    });
+    return () => unsubscribe();
+  }, [queryClient]);
+  return null;
+}
