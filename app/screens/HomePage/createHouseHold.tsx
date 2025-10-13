a68b8cd2a0846ad817a7e9e2d5bd826d1f9f2528
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-10-13 15:20:12 +0200

    Created function createHouseHold and switched positions on index.tsx HomePage and HousePage

---
+++ app/screens/HomePage/createHouseHold.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function createHouseHold() {
+  console.log("Hej create Household!!");
+  return (
+    <View style={{ backgroundColor: "black" }}>
+      <Text>Hej</Text>
+    </View>
+  );
+}
--- app/screens/HomePage/index.tsx
+++ app/screens/HomePage/index.tsx
@@ -1,45 +1,115 @@
-//Välj hushåll
 import { useAppTheme } from "@/constants/app-theme";
+// import { getAuth } from "firebase/auth";
 import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
-// Logged in page
-export default function HomeScreen() {
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import { Card, List, Text } from "react-native-paper";
+import { mockHouseholds } from "../../../src/data/mockdata";
+
+export default function householdScreen() {
   const theme = useAppTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: theme.colors.background,
+      height: "100%",
+      paddingTop: 20,
+    },
+    listContainer: {
+      flex: 8,
+    },
+    flatlist: {
+      height: "75%",
+    },
+    householdCard: {
+      margin: 10,
+      borderRadius: 15,
+    },
+    footer: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: "grey",
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    footerCard: {
+      margin: 10,
+      borderRadius: 25,
+      width: "40%",
+    },
+    footerCardContent: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: 10,
+    },
+  });
+
+  if (!mockHouseholds?.length) {
+    return <Text style={{ margin: 20 }}>Du har inga hushåll ännu.</Text>;
+  }
+  console.log("Nu är du i household");
+
   return (
-    //example with theme.
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: theme.colors.background,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <TouchableOpacity onPress={() => router.push("/screens/HousePage")}>
-        <Text
-          style={{
-            color: theme.colors.onPrimary,
-          }}
+    <View style={styles.container}>
+      <View style={styles.listContainer}>
+        <FlatList
+          style={styles.flatlist}
+          data={mockHouseholds} // Mockdata, här ska den inloggade personens hushåll läsas
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Card
+              style={styles.householdCard}
+              onPress={() => console.log("Öppnade hushåll:", item.name)}
+            >
+              <Card.Title
+                title={item.name}
+                subtitle={`Kod: ${item.generatedCode}`}
+                left={(props) => <List.Icon {...props} icon="home" />}
+              />
+            </Card>
+          )}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Card
+          style={styles.footerCard}
+          onPress={() => router.push("/screens/HomePage/createHouseHold")}
         >
-          Välj hushåll
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          padding: 20,
-          backgroundColor: theme.colors.primary,
-          borderRadius: 10,
-        }}
-        onPress={() => router.push("/screens/LoginPage")}
-      >
-        <Text
-          style={{
-            color: theme.colors.onPrimary,
-          }}
+          <Card.Content
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: 10,
+            }}
+          >
+            <Text variant="titleMedium">Lägg till</Text>
+            <List.Icon icon="plus" />
+          </Card.Content>
+        </Card>
+        <Card
+          style={styles.footerCard}
+          onPress={() => console.log("Nu ska du joina ett hem!")} //Här ska en modal öppnas för att skriva in en kod som leder till ett visst hushåll!
         >
-          Loginpage
-        </Text>
-      </TouchableOpacity>
+          <Card.Content style={styles.footerCardContent}>
+            <Text variant="titleMedium">Gå med </Text>
+            <List.Icon icon="plus" />
+          </Card.Content>
+        </Card>
+      </View>
     </View>
   );
 }
+
+//    TODO: Skapa en hook "Hooks/useHouseHolds.ts" där man hämtar användarens hushåll
+//------------------------------------------------------------------------------------
+// const auth = getAuth();
+// const user = auth.currentUser;
+// const { households, loading, error } = useHouseholds(user?.uid);
+// if (loading) return <Text style={{ margin: 20 }}>Laddar hushåll...</Text>;
+// if (error) return <Text style={{ margin: 20, color: "red" }}>{error}</Text>;
+//------------------------------------------------------------------------------------
