7ae56f0e625bb2ef0f8bcdcb8ad184de2046ddbd
Samuel.E <Sammy099@hotmail.se>
2025-10-14 10:53:29 +0200

    mockdata

--- app/screens/HouseholdPage/index.tsx
+++ app/screens/HouseholdPage/index.tsx
@@ -1,6 +1,40 @@
-import { useAppTheme } from '@/constants/app-theme';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, FlatList, TouchableOpacity, StyleSheet } from "react-native";
+import { useAppTheme } from "@/constants/app-theme";
+import { Card } from "react-native-paper";
+
+type mockChore = {
+    id: string;
+    title: string; 
+    description: string;
+    frequencyDats: number;
+    weight: number;
+    imageUrl?: string;
+    audioUrl?: string;
+    isArchived: boolean;
+    assignedTo: string; // Profile
+};
+
+const mockChores: mockChore[] = [
+    {
+        id: "1",
+        title: "Damma av",
+        description: "Damma av alla ytor i vardagsrummet.",
+        frequencyDats: 7,
+        weight: 3,
+        isArchived: false,
+        assignedTo: "user1",
+    },
+    {
+        id: "2",
+        title: "Dammsuga",
+        description: "Dammsuga hela huset.",
+        frequencyDats: 10,
+        weight: 5,
+        isArchived: false,
+        assignedTo: "user2",
+    }
+];
 
 export default function HouseholdPage() {
     const theme = useAppTheme();
